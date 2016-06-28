8053d2e39c2ab5985712097aa2b96c4de79e72b0
tobe <tobeg3oogle@gmail.com>
2016-06-28 07:29:42 +0000

    Add linear regression use checkpoint

---
+++ tensorflow_examples/linear_regression_use_checkpoint.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import tensorflow as tf 
+import numpy as np 
+
+train_X = np.linspace(-1, 1, 101)
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.33 + 10
+
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+w = tf.Variable(0.0, name="weight")
+b = tf.Variable(0.0, name="reminder")
+
+cost_op = tf.square(Y - tf.mul(X, w) - b)
+
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost_op)
+
+saver = tf.train.Saver()
+#saver = tf.train.Saver({"weight": w})
+
+with tf.Session() as sess:
+  tf.initialize_all_variables().run()
+
+  for i in range(10):
+  	for (x, y) in zip(train_X, train_Y):
+  	  sess.run(train_op, feed_dict={X: x, Y: y})
+
+  #saver.restore(sess, "./model.ckpt")
+
+  print(sess.run(w))
+  print(sess.run(b))
+
+  saver.save(sess, "./model.ckpt")
